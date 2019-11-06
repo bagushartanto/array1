5384f0e98a943bac6329cd091cf6e37717680223
bh1990 <56873720+bh1990@users.noreply.github.com>
2019-11-06 21:28:45 +0700

    Add files via upload

---
+++ array1/src/array1/Array1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array1;
+
+/**
+ *
+ * @author user
+ */
+public class Array1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] keluarga={"Bagus hartanto","Ulfatul zannah","M.aryasatya",};
+        for ( int i = 0; i < keluarga.length; i++){
+        System.out.println("jumlah keluarga sebanyak:"+keluarga.length);
+    }
+    }
+    
+}
